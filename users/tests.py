12350d67a153f1542e88fad08fa87cdf0ca6a7fd
jinyeobjeong <yeobdol@gmail.com>
2022-11-15 09:32:50 +0900

    init test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class UserRegisterationAPIViewTestCase(APITestCase):
+ def test_registration(self): 
+    url = reverse("user_view")
+    user_data = {
+    "username":"testuser",
+    "fullname":"테스터",
+    "email":"test@testuser.com",
+    "password":"password"
+    }
+    response = self.client.post(url, user_data)
+    self.assertEqual(response.data, {'message': '가입 완료!!'})
